26cd19303db75b09f9568f7dd9753c7563424034
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2025-04-16 21:14:27 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     new FastifyAdapter()
   );
   await app.register(cors, {
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     preflightContinue: false,
